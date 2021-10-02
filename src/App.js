89d238d55d5ea331e455219da529af0a93406f08
Vitaliy <vtk0679@gmail.com>
2021-10-03 00:38:53 +0300

    adds HW

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Statistics from "./components/Statistics";
+import Section from "./components/Section";
+import FeedbackOptions from "./components/FeedbackOptions";
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  ucFirst(str) {
+    if (!str) return str;
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  onLeaveFeedback = (e) => {
+    const value = e.target.value;
+    this.setState({ [value]: this.state[value] + 1 });
+  };
+
+  countTotalFeedback = () => {
+    return Object.keys(this.state).reduce(
+      (acc, key) => acc + this.state[key],
+      0
+    );
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const totalVotes = this.countTotalFeedback();
+    return totalVotes
+      ? Math.round((this.state.good * 100) / totalVotes)
+      : totalVotes;
+  };
+
+  render() {
+    const { good, bad, neutral } = this.state;
+    const totalVotes = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div className="App">
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalVotes}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      </div>
+    );
+  }
 }
 
 export default App;
